233169e264bf2943e0da1196de95de0d7b102ef9
biblius <crazycompanyxxl@gmail.com>
2026-06-21 13:02:52 +0200

    treesitter - switch to main branch

--- init.lua
+++ init.lua
@@ -9,10 +9,36 @@ vim.diagnostic.config { jump = { float = true } }
 
 -- Load options
 require 'options'
+
 -- Load dependencies
 require 'config.lazy'
+
 -- Initialise keymaps
 require 'keymap'
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {
+    'c',
+    'cpp',
+    'go',
+    'lua',
+    'python',
+    'rust',
+    'javascript',
+    'typescript',
+    'vimdoc',
+    'vim',
+    'bash',
+    'svelte',
+    'markdown',
+    'vue',
+    'xml',
+    'json',
+  },
+  callback = function()
+    vim.treesitter.start()
+  end,
+})
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
